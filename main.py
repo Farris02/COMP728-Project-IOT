bfd52e2c4cdd53f1bbe2ece12b1fc10e801628c9
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-04-29 11:26:34 +1200

    Added motor functions

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+'''
+The dependancies for the motor need to be downloaded here before starting the Python script:
+https://pypi.org/project/adafruit-circuitpython-motorkit/
+'''
+
 import RPi.GPIO as GPIO
 import datetime
 import time
 import boto3
+from adafruit_motorkit import MotorKit
 
 raindropSensor = 11 # Pin 11 / GPIO17 is the pin for the raindrop sensor
 humitureSensor = 12 # Pin 12 / GPIO18 is the pin for the humiture sensor
 infraredSensor = 13 # Pin 13 / GPIO27 is the pin for the infrared sensor
+'''
+relayOne = 15 # Pin 15 / GPIO22 is the pin for the first relay
+relayTwo = 16 # Pin 16 / GPIO23 is the pin for the second relay
+'''
 
 # Selects the mode for the board
 GPIO.setmode(GPIO.BOARD)
@@ -13,6 +23,15 @@ GPIO.setmode(GPIO.BOARD)
 # Sets up the inputs and outputs
 GPIO.setup(raindropSensor, GPIO.IN)
 GPIO.setup(infraredSensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+'''
+GPIO.setup(relayOne, GPIO.OUT)
+GPIO.output(relayOne, GPIO.LOW)
+GPIO.setup(relayTwo, GPIO.OUT)
+GPIO.output(relayTwo, GPIO.LOW)
+'''
+
+# Sets up the motor kit
+kit = MotorKit()
 
 # Humiture Constants
 MAX_UNCHANGE_COUNT = 100
@@ -21,6 +40,9 @@ STATE_INIT_PULL_UP = 2
 STATE_DATA_FIRST_PULL_DOWN = 3
 STATE_DATA_PULL_UP = 4
 STATE_DATA_PULL_DOWN = 5
+
+# Global variable to make sure that the motor isn't currently in use
+motorInUse = False;
 
 # Returns a 1 or a 0. 1 Being no rain and 0 being rain.
 def raindropSensorResult():
@@ -122,6 +144,71 @@ def humitureSensorResult():
 def infraredSensorResult():
     return GPIO.input(infraredSensor)
 
+#Pushing notifications to User
+def sendnotifications(message):
+    access_key = 'AKIA2ITYZKRINIP3IR4D'
+    secret_key = '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'
+    region = 'ap-southeast-2'
+    topic_arn = 'arn:aws:sns:ap-southeast-2:705701696592:Pushing-Notifications'
+
+    client = boto3.client('sns',aws_access_key_id=access_key,aws_secret_access_key=>
+
+    response = client.publish(TopicArn=topic_arn, Message=message)
+
+    print(response)
+
+# Open Window (Motor Shield)
+def openWindow():
+    if (!motorInUse):
+        motorInUse = True
+        kit.motor1.throttle = 0
+        time.sleep(0.5)
+        kit.motor1.throttle = 1.0
+        time.sleep(5)
+        kit.motor1.throttle = 0
+        time.sleep(0.5)
+        motorInUse = False
+
+# Close Window (Motor Shield)
+def closeWindow():
+    if (!motorInUse):
+        motorInUse = True
+        kit.motor1.throttle = 0
+        time.sleep(0.5)
+        while (infraredSensorResult() != 0):
+            kit.motor1.throttle = -1.0
+        kit.motor1.throttle = 0
+        time.sleep(0.5)
+        motorInUse = False
+
+'''
+# Open Window (Relay)
+def openWindow():
+    if (!motorInUse):
+        motorInUse = True
+        GPIO.output(relayOne, GPIO.LOW)
+        GPIO.output(relayTwo, GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(relayOne, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(relayOne, GPIO.LOW)
+        motorInUse = False
+
+# Close Window (Relay)
+def closeWindow():
+    if (!motorInUse):
+        motorInUse = True
+        GPIO.output(relayOne, GPIO.LOW)
+        GPIO.output(relayTwo, GPIO.LOW)
+        time.sleep(0.5)
+        # While the infrared sensor doesn't detect the wall, keep closing the window
+        while (infraredSensorResult() != 0):
+            GPIO.output(relayTwo, GPIO.HIGH)
+        GPIO.output(relayTwo, GPIO.LOW)
+        motorInUse = False
+'''
+
+# Data Loop
 def loop():
     printIt = False
     while True:
@@ -136,23 +223,9 @@ def loop():
             print("Window Closed: " + str("Yes" if infraredResult == 0 else "No") + ", Raining: " + str("Yes" if raining == 0 else "No") + ", Humidity: " + str(humidity) + "." + str(humidityDecimal) + "%, Temperature: " + str(temperature) + "." + str(temperatureDecimal) + "'C, Date and Time: " + str(currentTime))
         time.sleep(1.1)
 
+# Main
 if __name__ == "__main__":
     try:
         loop()
     except KeyboardInterrupt:
         GPIO.cleanup()
-
-#Pushing notifications to User
-def sendnotifications():
-access_key = 'AKIA2ITYZKRINIP3IR4D'
-secret_key = 'iVuO2MAUTjf/KcTdoI35uoPHnoEM6ruAplJP5Elk'
-region = 'ap-southeast-2'
-message = 'Please shut your windows'
-topic_arn = 'arn:aws:sns:ap-southeast-2:705701696592:Pushing-Notifications'
-
-client = boto3.client('sns',aws_access_key_id=access_key,aws_secret_access_key=>
-
-response = client.publish(TopicArn=topic_arn, Message=message)
-
-print(response) 
-
