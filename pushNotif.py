2a99be047eec4a180e83e2bcb69a7ea3af74e0ca
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-05-10 00:36:41 +1200

    Modifying sendNotifications

--- pushNotif.py
+++ pushNotif.py
@@ -3,14 +3,14 @@ import boto3
 
 # Pushing notifications to User
 def sendNotifications(message):
-    access_key = 'AKIA2ITYZKRINIP3IR4D'
-    secret_key = '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'
-    region = 'ap-southeast-2'
-    topic_arn = 'arn:aws:sns:ap-southeast-2:705701696592:Pushing-Notifications'
-    client = boto3.client('sns',aws_access_key_id=access_key,aws_secret_access_key=secret_key, region_name=region)
-    response = client.publish(TopicArn=topic_arn, Message=message)
-    print(response)
-    print(message)
+  access_key = 'AKIA5QGOG4BAVSIS4GHD'
+  secret_key = '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'
+  region = 'ap-southeast-2'
+  topic_arn = 'arn:aws:sns:ap-southeast-2:928145399873:Push-Notifications-IOT'
+  client = boto3.client('sns',aws_access_key_id= access_key, aws_secret_access_key = secret_key,region_name = region)
+  response = client.publish(TopicArn = topic_arn, Message = message)
+  print(response)
+  print(message)
 
 # Continuously checks the sendNotifications file for any new notifications
 def loop():
